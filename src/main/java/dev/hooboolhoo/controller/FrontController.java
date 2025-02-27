40d07ca7e1ba1d5b429faf9c8085b08ee0811e8b
Yoon young chan <119031366+yyc0026@users.noreply.github.com>
2025-02-27 14:35:01 +0900

    FrontController 수정

--- src/main/java/dev/hooboolhoo/controller/FrontController.java
+++ src/main/java/dev/hooboolhoo/controller/FrontController.java
@@ -1,6 +1,10 @@
 package dev.hooboolhoo.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,6 +30,20 @@ public class FrontController extends HttpServlet {
     private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uri = request.getRequestURI();
         String[] uriParts = uri.split("/");
+        
+        
+        try {
+        	
+        	 String DB_URL = "jdbc:mysql://192.168.0.216:3306/";
+        	String DATABASE_NAME = "hooboolhoo";
+        	String USER = "root";
+        	 String PASSWORD = "021326cc";
+        	 String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+        	 Class.forName(DRIVER_NAME);
+        	Connection connection = DriverManager.getConnection(DB_URL + DATABASE_NAME,USER,PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+        	e.printStackTrace();
+        }
 
         // 요청 URI에 따라 처리할 서블릿 선택
         if (uriParts.length > 1) {
